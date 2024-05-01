06766e67553acf532288944083a039d321e5a232
josek98 <josemmsscc98@gmail.com>
2024-05-01 13:25:09 +0200

    Correct the setup.py file

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages #type: ignore
 
 with open("README.md", "r",) as f: 
     long_description = f.read()
@@ -7,9 +7,14 @@ setup(
     name="pywgraph",
     version="0.0.1",
     description="A python implementation of weighted directed graphs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="josek98",
     author_email="josemmsscc98@gmail.com",
     packages=find_packages(),
     python_requires=">=3.10",
     install_requires=[],
+    extras_require={
+        "dev": ["pytest>=7.0", "twine>=4.0.2"]
+    }
 )
