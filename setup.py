7292a8569229ad240130fa6bb12f2b5ca3f319c5
Jose <134541650+josek98@users.noreply.github.com>
2024-05-02 18:24:55 +0200

    Update version for release on setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r",) as f:
 
 setup(
     name="pywgraph",
-    version="0.1.0",
+    version="0.2.0",
     description="A python implementation of weighted directed graphs",
     long_description=long_description,
     long_description_content_type="text/markdown",
