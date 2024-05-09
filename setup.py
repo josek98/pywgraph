080f672fd1f66500af8ef09fd69a257df5d17fc9
josek98 <josemmsscc98@gmail.com>
2024-05-09 23:20:52 +0200

    update setup and reame for release 1.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r",) as f:
 
 setup(
     name="pywgraph",
-    version="1.0.1",
+    version="1.0.2",
     description="A python implementation of weighted directed graphs",
     long_description=long_description,
     long_description_content_type="text/markdown",
