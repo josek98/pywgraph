b535afaeff1521a1de27c480a14581abb4847619
josek98 <josemmsscc98@gmail.com>
2024-05-15 11:44:01 +0200

    add tests for paths and cycles

--- tests/test_paths_and_cycles.py
+++ tests/test_paths_and_cycles.py
@@ -8,11 +8,8 @@ def list_path() -> list[str]:
 def list_cycle() -> list[str]:
     return ["A", "B", "C", "A"]
 
-def list_cycle_with_duplicates() -> list[str]:
-    return ["A", "B", "C", "A", "B","A"]
-
-def list_path_with_duplicates() -> list[str]:
-    return ["A", "B", "C", "D", "A", "B"]
+def list_equivalent_cycle() -> list[str]:
+    return ["C","A", "B", "C"]
 
 def list_invalid_path() -> list[str]:
     return ["A", "B", "B"]
@@ -24,6 +21,10 @@ class TestPathsAndCycles:
         with pytest.raises(ValueError):
             Path([])
 
+    def test_invalid_path(self):
+        with pytest.raises(ValueError):
+            Path(list_invalid_path())
+
     def test_empty_cycle(self):
         with pytest.raises(ValueError):
             Cycle([])
@@ -31,3 +32,17 @@ class TestPathsAndCycles:
     def test_not_cycle(self):
         with pytest.raises(ValueError):
             Cycle(list_path())
+
+    def test_cycle_from_path(self):
+        path = Path(list_cycle())
+        Cycle(path)
+
+    def test_equal_cycles(self): 
+        assert Cycle(list_cycle()) == Cycle(list_equivalent_cycle())
+
+    def test_path_is_cycle(self):
+        assert Path(list_cycle()).is_cycle
+
+    def test_path_is_not_cycle(self):
+        assert not Path(list_path()).is_cycle
+    
